fb1f36f10ae2506d28262f2dacac716e695a7b5c
Veturia Foltutiu <anca.ulici71@gmail.com>
2023-04-11 19:17:52 +0300

    fetch current weather date from RapidApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './services/weather.service';
+import { CurrentWeather } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,34 @@ export class AppComponent implements OnInit {
 
   }
 
+  cityName = 'Oradea';
+  currentWeather?: CurrentWeather;
+
   ngOnInit(): void {
-    this.weatherService.getCurrentWeather('Oradea')
+    this.getCurrentWeather(this.cityName);
+  }
+
+  onSubmit(){
+    this.getCurrentWeather(this.cityName);
+    this.cityName = '';
+
+  }
+
+  private getCurrentWeather(cityName: string) {
+    this.weatherService.getCurrentWeather(cityName)
     .subscribe({
       next: (response) => {
+        this.currentWeather = response;
+        this.currentWeather.main.temp = this.transformFahrenheitToCelsius (this.currentWeather.main.temp);
+        this.currentWeather.main.temp_min = this.transformFahrenheitToCelsius (this.currentWeather.main.temp_min);
+        this.currentWeather.main.temp_max = this.transformFahrenheitToCelsius (this.currentWeather.main.temp_max);
         console.log(response);
-        response.main.temp
       }
-    })
+    });
   }
+  transformFahrenheitToCelsius(fahrenheit: number): number {
+    const celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
+  }
+  
 }
